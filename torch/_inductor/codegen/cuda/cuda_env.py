758cea557a30855eb8aa66c550354daf0eeb98da
Ying Zhang <yingz@meta.com>
2023-08-25 23:27:08 -0700

    CUDAcodecache
    ghstack-source-id: 006b428d8bd12366db687da36fc5ee84b375b8c3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107847

--- torch/_inductor/codegen/cuda/cuda_env.py
+++ torch/_inductor/codegen/cuda/cuda_env.py
@@ -30,3 +30,9 @@ def get_cuda_version() -> Optional[str]:
     except Exception as e:
         logging.error(f"Error getting cuda version: {e=}")
         return None
+
+
+def nvcc_exist() -> bool:
+    import subprocess
+    res = subprocess.call(["which", "nvcc"])
+    return res == 0
